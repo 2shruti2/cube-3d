4675b8037cb2a26c87e629901de42a08c17c0c75
2shruti2 <shruthesharma@gmail.com>
2024-05-23 23:55:20 +0530

    ui: assets and drag added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import Cube from "./components/Cube";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
   return (
-    <>
+    <div className="h-[100vh] bg-zinc-50">
       <Canvas>
+        <ambientLight intensity={2} />
+        <directionalLight position={[2, 1, 1]} />
+        <OrbitControls enableZoom={false} />
         <Cube />
       </Canvas>
-    </>
+    </div>
   );
 }
 
--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -1,7 +1,32 @@
+import { useFrame, useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+import { useRef } from "react";
+
 const Cube = () => {
+  const mesh = useRef(null);
+
+  useFrame((state, delta) => {
+    mesh.current.rotation.x += delta * 0.25;
+    mesh.current.rotation.y += delta * 0.25;
+    mesh.current.rotation.z += delta * 0.25;
+  });
+
+  const texture_1 = useLoader(TextureLoader, "/assets/1.jpg");
+  const texture_2 = useLoader(TextureLoader, "/assets/2.jpg");
+  const texture_3 = useLoader(TextureLoader, "/assets/3.jpg");
+  const texture_4 = useLoader(TextureLoader, "/assets/4.jpg");
+  const texture_5 = useLoader(TextureLoader, "/assets/5.jpg");
+  const texture_6 = useLoader(TextureLoader, "/assets/6.jpg");
+
   return (
-    <mesh>
-      <boxGeometry />
+    <mesh ref={mesh}>
+      <boxGeometry args={[3, 3, 3]} />
+      <meshStandardMaterial map={texture_1} attach={"material-0"} />
+      <meshStandardMaterial map={texture_2} attach={"material-1"} />
+      <meshStandardMaterial map={texture_3} attach={"material-2"} />
+      <meshStandardMaterial map={texture_4} attach={"material-3"} />
+      <meshStandardMaterial map={texture_5} attach={"material-4"} />
+      <meshStandardMaterial map={texture_6} attach={"material-5"} />
     </mesh>
   );
 };
