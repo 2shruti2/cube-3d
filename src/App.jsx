bb12a32b789676b7ceeeb6b5086c94e548ac6a9f
2shruti2 <shruthesharma@gmail.com>
2024-05-24 01:16:42 +0530

    ui: scroll animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import { Canvas } from "@react-three/fiber";
 import Cube from "./components/Cube";
 import { OrbitControls } from "@react-three/drei";
+import { useScroll, useSpring, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 function App() {
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
+
+  const progress = useTransform(scrollYProgress, [0, 1], [0, 5]);
+  const smoothProgress = useSpring(progress, { damping: 20 });
+
   return (
-    <div className="h-[100vh] bg-zinc-50">
-      <Canvas>
-        <ambientLight intensity={2} />
-        <directionalLight position={[2, 1, 1]} />
-        <OrbitControls enableZoom={false} />
-        <Cube />
-      </Canvas>
+    <div ref={container} className="h-[100vh] bg-zinc-50 main relative">
+      <div className="cube">
+        <Canvas>
+          <ambientLight intensity={2} />
+          <directionalLight position={[2, 1, 1]} />
+          <OrbitControls enableZoom={false} enablePan={false} />
+          <Cube progress={smoothProgress} />
+        </Canvas>
+      </div>
     </div>
   );
 }
--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -1,15 +1,46 @@
 import { useFrame, useLoader } from "@react-three/fiber";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+import { useMotionValue, useSpring } from "framer-motion";
+import { motion } from "framer-motion-3d";
+import { useScroll } from "@react-three/drei";
 
-const Cube = () => {
+const Cube = ({ progress }) => {
   const mesh = useRef(null);
 
-  useFrame((state, delta) => {
-    mesh.current.rotation.x += delta * 0.25;
-    mesh.current.rotation.y += delta * 0.25;
-    mesh.current.rotation.z += delta * 0.25;
-  });
+  const data = useScroll();
+
+  const options = {
+    damping: 20,
+  };
+
+  const mouse = {
+    x: useSpring(useMotionValue(0), options),
+    y: useSpring(useMotionValue(0), options),
+  };
+
+  const manageMouseMove = (e) => {
+    const { innerWidth, innerHeight } = window;
+    const { clientX, clientY } = e;
+    const x = clientX / innerWidth - 0.5;
+    const y = clientY / innerHeight - 0.5;
+    mouse.x.set(x);
+    mouse.y.set(y);
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", manageMouseMove);
+
+    return () => {
+      window.addEventListener("mousemove", manageMouseMove);
+    };
+  }, []);
+
+  // useFrame((state, delta) => {
+  //   mesh.current.rotation.x += delta * 0.25;
+  //   mesh.current.rotation.y += delta * 0.25;
+  //   mesh.current.rotation.z += delta * 0.25;
+  // });
 
   const texture_1 = useLoader(TextureLoader, "/assets/1.jpg");
   const texture_2 = useLoader(TextureLoader, "/assets/2.jpg");
@@ -19,15 +50,15 @@ const Cube = () => {
   const texture_6 = useLoader(TextureLoader, "/assets/6.jpg");
 
   return (
-    <mesh ref={mesh}>
-      <boxGeometry args={[3, 3, 3]} />
+    <motion.mesh ref={mesh} rotation-y={progress} rotation-x={progress}>
+      <boxGeometry args={[2.5, 2.5, 2.5]} />
       <meshStandardMaterial map={texture_1} attach={"material-0"} />
       <meshStandardMaterial map={texture_2} attach={"material-1"} />
       <meshStandardMaterial map={texture_3} attach={"material-2"} />
       <meshStandardMaterial map={texture_4} attach={"material-3"} />
       <meshStandardMaterial map={texture_5} attach={"material-4"} />
       <meshStandardMaterial map={texture_6} attach={"material-5"} />
-    </mesh>
+    </motion.mesh>
   );
 };
 
